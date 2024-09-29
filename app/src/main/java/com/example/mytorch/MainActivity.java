4e19c56a10d103673a1f9d1ced9d5e8c9e6623d6
pradiptars <pradiptars@gmail.com>
2024-09-29 22:43:27 +0600

    first commit

---
+++ app/src/main/java/com/example/mytorch/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.mytorch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
